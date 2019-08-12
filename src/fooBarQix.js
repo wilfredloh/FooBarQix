ba57bfb8258cd90841c59e6a547d0e05dc48d4d1
Min Shan <min.shan@online-pajak.com>
2019-08-12 15:16:17 +0800

    Write validation for param being a number

--- src/fooBarQix.js
+++ src/fooBarQix.js
@@ -1,5 +1,14 @@
 function compute(number) {
+	validate(number);
 	return number;
+}
+
+function validate(parameter) {
+	if (!isNumber(parameter)) throw new Error(`"${parameter}" must be a number`);
+}
+
+function isNumber(parameter) {
+	return typeof parameter === 'number';
 }
 
 module.exports = {
--- test/fooBarQix.spec.js
+++ test/fooBarQix.spec.js
@@ -6,5 +6,10 @@ describe('FooBarQix', () => {
 		it('should return 1 when the input is 1', () => {
 			expect(fooBarQix.compute(1)).to.be.equal(1);
 		});
+
+		it('should throw an error if the input is not a number', () => {
+			expect(() => fooBarQix.compute('1')).to.throw('"1" must be a number');
+			expect(() => fooBarQix.compute(true)).to.throw('"true" must be a number');
+		});
 	});
 });
