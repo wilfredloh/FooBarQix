d01e2b87b679303c312a92beeba27b30288cc922
wilfredloh <lohwilfred19@gmail.com>
2019-08-30 13:28:18 +0800

    testing out TDD exercise + refactoring

--- src/FooBarQix.js
+++ src/FooBarQix.js
@@ -1,7 +1,34 @@
-function helloWorld() {
-    return 'Hello world!';
+// function helloWorld() {
+//     return 'Hello world!';
+// }
+
+function checkNum(num) {
+    let message = ''
+    let numArr = [3, 5, 7];
+    let stringNumArr = ['3', '5', '7'];
+    let messageArr = ['Foo', 'Bar', 'Qix']
+
+    for (let i=0; i<numArr.length; i++) {
+        if (num % numArr[i] === 0){
+            message += messageArr[i];
+        }
+        let stringNum = num.toString();
+        // let splittednum = num.toString().split("")
+
+        // if (splittednum[0] === splittednum[1]){
+        //     message += messageArr[i];
+        // }
+        if (stringNum.includes(stringNumArr[i])) {
+            message += messageArr[i];
+        }
+    }
+    if (message !== ''){
+        return message;
+    }
+    return num;
 }
 
 module.exports = {
-    helloWorld
+    // helloWorld,
+    checkNum
 };
--- test/FooBarQix.test.js
+++ test/FooBarQix.test.js
@@ -2,9 +2,46 @@ const { expect } = require('chai');
 const FooBarQix = require('../src/FooBarQix');
 
 describe('FooBarQix', () => {
-    describe('.helloWorld()', () => {
-        it('should return the string "Hello world!"', () => {
-            expect(FooBarQix.helloWorld()).to.equal('Hello world!');
+    describe('.checkNum()', () => {
+        // it('should return the string "Hello world!"', () => {
+        //     expect(FooBarQix.helloWorld()).to.equal('Hello world!');
+        // });
+
+        it('should return the num', () => {
+            expect(FooBarQix.checkNum(1)).to.equal(1);
+        });
+        it('should return the num', () => {
+            expect(FooBarQix.checkNum(2)).to.equal(2);
+        });
+        it('should return the string "FooFoo"', () => {
+            expect(FooBarQix.checkNum(3)).to.equal('FooFoo');
+        });
+        it('should return the num', () => {
+            expect(FooBarQix.checkNum(4)).to.equal(4);
+        });
+        it('should return the string "BarBar"', () => {
+            expect(FooBarQix.checkNum(5)).to.equal('BarBar');
+        });
+        it('should return the string "Foo"', () => {
+            expect(FooBarQix.checkNum(6)).to.equal('Foo');
+        });
+        it('should return the string "QixQix"', () => {
+            expect(FooBarQix.checkNum(7)).to.equal('QixQix');
+        });
+        it('should return the string "Bar"', () => {
+            expect(FooBarQix.checkNum(10)).to.equal('Bar');
+        });
+        it('should return the string "Foo"', () => {
+            expect(FooBarQix.checkNum(13)).to.equal('Foo');
+        });
+        it('should return the string "FooBarBar"', () => {
+            expect(FooBarQix.checkNum(15)).to.equal('FooBarBar');
+        });
+        it('should return the string "FooFooFoo"', () => {
+            expect(FooBarQix.checkNum(33)).to.equal('FooFooFoo');
+        });
+        it('should return the string "FooBarQix"', () => {
+            expect(FooBarQix.checkNum(1357)).to.equal('FooBarQix');
         });
     });
 });
